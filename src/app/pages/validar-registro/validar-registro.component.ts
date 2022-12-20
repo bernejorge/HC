0c213e39bb15f8c9024c87e1793d07146486136e
bernejorge <bernejorge@gmail.com>
2022-12-20 18:02:14 -0300

    Modal_Registrase_desde_Validacion

--- src/app/pages/validar-registro/validar-registro.component.ts
+++ src/app/pages/validar-registro/validar-registro.component.ts
@@ -3,6 +3,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import Swal from 'sweetalert2';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { RegistrarseComponent } from '../login-inicio/modales/registrarse/registrarse.component';
+import { RegistroExitosoComponent } from '../login-inicio/modales/registro-exitoso/registro-exitoso.component';
 @Component({
   selector: 'app-validar-registro',
   templateUrl: './validar-registro.component.html',
@@ -13,7 +16,7 @@ export class ValidarRegistroComponent implements OnInit {
   cForm: FormGroup;
 
 
-  constructor(private loginSrv: LoginService, private fb: FormBuilder, private router: Router) { 
+  constructor(private loginSrv: LoginService, private fb: FormBuilder, private router: Router, private modalService: NgbModal) { 
     this.cForm = this.fb.group(
       {
         digito1: ['', Validators.compose([Validators.required, Validators.maxLength(1)])],
@@ -70,6 +73,68 @@ export class ValidarRegistroComponent implements OnInit {
       this.router.navigate(['/generar-password']);
     })
   }
+
+  openModal(){
+    const modalRegRef = this.modalService.open(RegistrarseComponent);
+			modalRegRef.result.then((result) => {
+				if (result) {
+	
+					const doc = result.controls.docnumber.value;
+					const nac = new Date(result.controls.nac.value);
+					this.regitrarse(doc, nac);
+				}
+			}, (reason)=>{
+				console.log(reason);
+				//this.setRegistro();
+			}
+			);
+  }
+  regitrarse(doc: string, nac: Date) {
+		//definicion del observador
+		const RegistrarseObserver = {
+			next: (x) => {
+				//respuesta de exito
+				console.log(x);
+				//mostrar mensaje de exito, mostrar modal
+				const mail = this.obscure_email(x.Email);
+				const modalRef = this.modalService.open(RegistroExitosoComponent)
+				modalRef.componentInstance.email = mail.toLowerCase();
+				modalRef.result.then((result) => {
+					this.router.navigate(['/validar-registro']);
+
+				}, (reason) => {
+					console.log(reason);
+					this.router.navigate(['/validar-registro']);
+				});
+
+				;
+			},
+
+		}
+
+		this.loginSrv.registarse("1", doc, nac.toISOString())
+			.subscribe(RegistrarseObserver);
+	}
+  obscure_email(email) {
+		var parts = email.split("@");
+		var name = parts[0];
+		var result = name.charAt(0);
+		for (var i = 1; i < name.length; i++) {
+			result += "*";
+		}
+		result += name.charAt(name.length - 1);
+		result += "@";
+		var domain = parts[1];
+		result += domain.charAt(0);
+		var dot = domain.indexOf(".");
+		for (var i = 1; i < dot; i++) {
+			result += "*";
+		}
+		result += domain.substring(dot);
+
+		return result;
+	}
+
   msgValidacionExitoConPerfil() {
     Swal({
       title: 'Valdacion Exitosa',

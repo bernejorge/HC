f2cb9d6c7d7d1a5fd6af177b70e619504b40f5f7
bernejorge <bernejorge@gmail.com>
2022-12-26 18:43:24 -0300

    OBS 7

--- src/app/pages/hc/internaciones/internacion-modal/internacion-modal.component.ts
+++ src/app/pages/hc/internaciones/internacion-modal/internacion-modal.component.ts
@@ -17,5 +17,15 @@ export class InternacionModalComponent implements OnInit {
 
   ngOnInit() {
   }
+  imprimir() {
+    // const printContent = document.getElementById("printThis");
+    // const WindowPrt = window.open('', '', 'left=0,top=0,width=900,height=900,toolbar=0,scrollbars=0,status=0');
+    // WindowPrt.document.write(printContent.innerHTML);
+    // WindowPrt.document.close();
+    // WindowPrt.focus();
+    // WindowPrt.print();
+    // WindowPrt.close();
+    window.print();
+  }
 
 }
